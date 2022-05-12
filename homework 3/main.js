1f19a98fca08845962d69c5a2e2b8eb60a198b3b
Yedil Dinmukhamed <54952217+dxrk23@users.noreply.github.com>
2022-05-13 00:03:12 +0600

    Small code-style fixes.

--- homework 3/main.js
+++ homework 3/main.js
@@ -1,19 +1,19 @@
-Array.prototype.myFilter = function(callback, obj) {
-  if (obj === undefined) {
-    obj = this;
-  }
-  let newArray = [];
+Array.prototype.myFilter = function(callback, obj = this) {
+  const newArray = [];
+  
   for (let i = 0; i < obj.length; i++) {
     if (callback(obj[i])) {
       newArray.push(obj[i]);
     }
   }
+  
   return newArray;
 }
 
 
 function createDebounceFunction(func, wait) {
   let timeout;
+  
   return function() {
     const context = this;
     const args = arguments;
