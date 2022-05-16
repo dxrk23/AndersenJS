e0f6f21b6afdfe57a7a97237d54c72c4364b38cd
dxrk23 <dinmukhamedyedil@gmail.com>
2022-05-16 21:51:10 +0600

    homework 5

--- homework 5/stack.js
+++ homework 5/stack.js
@@ -1,45 +1,122 @@
-class Stack {
-  constructor(size = 10) {
-    this.size = size;
-    this.stack = new Array(size);
-    this.top = -1;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  push(value) {
-    if (this.top === this.size - 1) {
-      throw new Error('Stack is full');
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
     }
 
-    this.stack[++this.top] = value;
+    this.length++;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.length++;
   }
 
   pop() {
-    if (this.top === -1) {
+    if (!this.head) {
+      return null;
+    }
+
+    const node = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = node.prev;
+      this.tail.next = null;
+      node.prev = null;
+    }
+
+    this.length--;
+
+    return node.value;
+  }
+}
+
+class Stack {
+  constructor(size = 10) {
+    this.size = size;
+    this.list = new LinkedList();
+  }
+
+  push(value) {
+    if (this.list.length === this.size) {
+      throw new Error('Stack is full');
+    }
+
+    this.list.append(value);
+  }
+
+  pop() {
+    if (this.list.length === 0) {
       throw new Error('Stack is empty');
     }
 
-    const value = this.stack[this.top--];
-
-    return value;
+    return this.list.pop();
   }
 
   peek() {
-    if (this.top === -1) {
-      throw new Error('Stack is empty');
+    if (this.list.length === 0) {
+      return null;
     }
 
-    return this.stack[this.top];
+    return this.list.tail.value;
   }
 
   isEmpty() {
-    return this.top === -1;
+    return this.list.length === 0;
   }
 
   toArray() {
-    return this.stack.slice(0, this.top + 1);
+    const array = [];
+
+    let node = this.list.head;
+
+    while (node !== null) {
+      array.push(node.value);
+      node = node.next;
+    }
+
+    return array;
   }
 
   static fromIterable(iterable) {
+    if (!iterable[Symbol.iterator]) {
+      throw new Error('Object is not iterable');
+    }
+
     const stack = new Stack();
 
     for (const value of iterable) {
@@ -49,5 +126,3 @@ class Stack {
     return stack;
   }
 }
-
-module.exports = { Stack };
